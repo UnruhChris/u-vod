0d7c8df1768d0d4637cad32339097fff589c15df
UnruhChris <gambardella.chriss00@gmail.com>
2026-02-18 15:10:58 +0100

    gateway fixes

--- backend/user/src/main/java/com/uvod/user/config/UserCosmosConfig.java
+++ backend/user/src/main/java/com/uvod/user/config/UserCosmosConfig.java
@@ -2,11 +2,16 @@ package com.uvod.user.config;
 
 import com.azure.cosmos.CosmosClientBuilder;
 import com.azure.cosmos.GatewayConnectionConfig;
+import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.spring.data.cosmos.config.AbstractCosmosConfiguration;
 import com.azure.spring.data.cosmos.repository.config.EnableCosmosRepositories;
+
+import ch.qos.logback.core.util.StringUtil;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 @Configuration
 @EnableCosmosRepositories(basePackages = "com.uvod.user.repository")
@@ -15,7 +20,7 @@ public class UserCosmosConfig extends AbstractCosmosConfiguration {
     @Value("${AZURE_COSMOS_URI}")
     private String uri;
 
-    @Value("${AZURE_COSMOS_KEY}")
+    @Value("${AZURE_COSMOS_KEY:}")
     private String key;
 
     @Value("${AZURE_COSMOS_DB}")
@@ -28,6 +33,12 @@ public class UserCosmosConfig extends AbstractCosmosConfiguration {
 
     @Bean
     public CosmosClientBuilder cosmosClientBuilder() {
+        if (!StringUtils.hasText(key)) {
+            // Use Managed Identity
+            return new CosmosClientBuilder()
+                    .endpoint(uri)
+                    .credential(new DefaultAzureCredentialBuilder().build());
+        }
         GatewayConnectionConfig gatewayConfig = new GatewayConnectionConfig();
 
         return new CosmosClientBuilder()
